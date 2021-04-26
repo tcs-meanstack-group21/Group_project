a0e55b5aee21ea5264f18d7f26058b8207773d8b
matthew-earl-barnes <matthewearlbarnes@gmail.com>
2021-04-26 10:53:17 -0700

    Added basic cart get, add item, and remove item

---
+++ backEnd/controller/customer.controller.js
@@ -0,0 +1,53 @@
+const ProductModel = require("../model/product.model")
+const CustomerModel = require("../model/customer.model")
+
+// in Angular, use pid for product._id and uid for user._id
+const getCart = (req, res) => {
+    const userId = req.params.uid;
+    CustomerModel.find({ _id: userId }, (err, data) => {
+        if (!err) {
+            let user = data[0];
+            let cartKeys = [...user.cart.keys()];
+            ProductModel.find({ _id: { "$in": cartKeys } }, (err2, result) => {
+                if (!err2) res.json(result);
+                else res.send(err2);
+            })
+        }
+    })
+}
+
+const addProductToCart = (req, res) => {
+    const productId = req.body.pid.toString();
+    const userId = req.body.uid;
+    const addQuantity = req.body.quantity;
+    CustomerModel.find({ _id: userId }, (err, data) => {
+        if (!err) {
+            let user = data[0];
+            if (user) {
+                const oldQuantity = user.cart.get(productId) || 0;
+                user.cart.set(productId, oldQuantity + addQuantity);
+                res.send("Successfully added item");
+            } else {
+                res.send("Error: no user found");
+            }
+        } else {
+            res.send("Error generated: " + err);
+        }
+    })
+}
+
+const removeProductFromCart = (req, res) => {
+    const productId = req.body.pid.toString();
+    const userId = req.body.uid;
+    CustomerModel.find({ _id: userId }, (err, data) => {
+        if (!err) {
+            let user = data[0];
+            if (user) {
+                user.cart.delete(productId);
+                res.send("Item deleted")
+            }
+        } else res.send("An error occured")
+    })
+}
+
+module.exports = { getCart, addProductToCart, removeProductFromCart }
