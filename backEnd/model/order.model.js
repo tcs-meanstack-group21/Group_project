d0621ccb859b932c3c12421dfeadfa37ecaef5fa
matthew-earl-barnes <matthewearlbarnes@gmail.com>
2021-04-26 11:03:16 -0700

    Added order model

---
+++ backEnd/model/order.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+    _id: Number,
+    customer: Schema.Types.ObjectId,
+    cart: { type: Map, of: Number },
+    dateOrdered: { type: Date, default: Date.now() },
+    dateDelivered: Date,
+    cancellationReason: { type: String, default: "" }
+});
+
+const OrderModel = mongoose.model("", OrderSchema, "orders");
+
+module.exports = OrderModel;
