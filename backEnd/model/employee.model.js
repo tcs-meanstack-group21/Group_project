7c3cd8ab7a791429eac6c81d977e47564103c15a
Jzzzgg <45835787+Jzzzgg@users.noreply.github.com>
2021-04-26 14:51:49 -0400

    add last log time

--- backEnd/model/employee.model.js
+++ backEnd/model/employee.model.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 
 const EmployeeSchema = new Schema({
     _id: Number,
-    password : {type : String}
+    password : {type : String, required : true},
+    lastLogTime : {type : Date}
 });
 
 const EmployeeModel = mongoose.model("", EmployeeSchema, "employee");
