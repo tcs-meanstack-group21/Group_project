d6c3f941b180075204c2b4ee19f6a563cbfefa31
Katrina Thielen <Thiel433@d.umn.edu>
2021-04-27 15:48:09 -0500

    Add Employee Order Status Component and organize all Employee Components

--- frontEnd/grocers-app/src/app/app-routing.module.ts
+++ frontEnd/grocers-app/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
-import { EmployeeRequestComponent } from './employee-request/employee-request.component';
+import { EmployeeDashboardComponent } from './employee/employee-dashboard/employee-dashboard.component';
+import { EmployeeOrderStatusComponent } from './employee/employee-order-status/employee-order-status.component';
+import { EmployeeRequestComponent } from './employee/employee-request/employee-request.component';
 import { InitComponent } from './init/init.component';
 import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 
@@ -10,7 +11,8 @@ const routes: Routes = [
   {path: "\init", component : InitComponent},
   {path: "", redirectTo : "\init", pathMatch:"full"},
   {path:"\empDash", component: EmployeeDashboardComponent}, // Employee - Dashboard
-  {path:"\empReq", component: EmployeeRequestComponent} // Employee - Send Request
+  {path:"\empReq", component: EmployeeRequestComponent}, // Employee - Send Request
+  {path:"\empOrd", component: EmployeeOrderStatusComponent} // Employee - Update Order Status
 ];
 
 @NgModule({
--- frontEnd/grocers-app/src/app/app.module.ts
+++ frontEnd/grocers-app/src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material Imports
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
+// Neutral Components
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 import { InitComponent } from './init/init.component';
-import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule } from '@angular/material/button';
-import { EmployeeRequestComponent } from './employee-request/employee-request.component';
+// Employee Components
+import { EmployeeDashboardComponent } from './employee/employee-dashboard/employee-dashboard.component';
+import { EmployeeRequestComponent } from './employee/employee-request/employee-request.component';
+import { EmployeeOrderStatusComponent } from './employee/employee-order-status/employee-order-status.component';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +26,8 @@ import { EmployeeRequestComponent } from './employee-request/employee-request.co
     SignUpPageComponent,
     InitComponent,
     EmployeeDashboardComponent,
-    EmployeeRequestComponent
+    EmployeeRequestComponent,
+    EmployeeOrderStatusComponent
   ],
   imports: [
     BrowserModule,
