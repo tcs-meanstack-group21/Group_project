e19e6bf0fc605bacdcc5bd3c8548e0c14c2daa4d
shraddhadhyade <shraddhadhyade@gmail.com>
2021-04-29 00:47:33 -0700

    Added Admin DashBoard features

--- frontEnd/grocers-app/src/app/admin/admin.component.ts
+++ frontEnd/grocers-app/src/app/admin/admin.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
+
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(public proService:ProductService) { }
 
   ngOnInit(): void {
   }
