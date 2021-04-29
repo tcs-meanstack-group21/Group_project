403817eb5f7846294a37844d785cf39ee26d5f5c
shraddhadhyade <shraddhadhyade@gmail.com>
2021-04-29 13:33:58 -0700

    Update Products table after every change

--- frontEnd/grocers-app/src/app/admin/admin.component.ts
+++ frontEnd/grocers-app/src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../model.product';
 import { ProductService } from '../product.service';
 
 @Component({
@@ -9,9 +10,19 @@ import { ProductService } from '../product.service';
 
 export class AdminComponent implements OnInit {
 
+  products:Array<Product> = [];
   constructor(public proService:ProductService) { }
 
   ngOnInit(): void {
+    this.proService.retrieveProductDetails().subscribe(result=>this.products=result)
+  }
+
+  onTabChanged($event:any){
+    this.proService.retrieveProductDetails().subscribe(result=>this.products=result)
+  }
+
+  updateProducts(){
+    this.proService.retrieveProductDetails().subscribe(result=>this.products=result)
   }
 
 }
