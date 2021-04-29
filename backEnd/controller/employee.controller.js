aaa674c26816c590f126a8b9edff4cc125e87c24
ujwala chebrolu <ujwalachebrolu153@gmail.com>
2021-04-29 10:31:20 -0700

    merged addemployee controller and model to emmployee

--- backEnd/controller/employee.controller.js
+++ backEnd/controller/employee.controller.js
@@ -1,6 +1,6 @@
 
-const employeeModel = require("../model/employee.model")
-
+const employeeModel = require("../model/employee.model");
+const EmployeeModel = require("../model/employee.model.js");
 let empSignIn = (req,res) =>{
     const eid = eval(req.body.user);
     const pass = req.body.pass;
@@ -33,4 +33,41 @@ employee.save((err,result)=> {
 })
 }
 
-module.exports = {empSignIn, empSignUp}
+let addEmployeeInfo = (req,res)=> {
+   
+    let employee = new EmployeeModel({
+        
+        firstname:req.body.firstname,
+        lastname:req.body.lastname,
+        emailid:req.body.emailid,
+
+    });
+
+    employee.save((err,result)=> {
+        if(!err){
+            res.send("Record stored successfully ")
+            //res.json({"msg":"Record stored successfully"})
+        }else {
+            res.send("Record didn't store ");
+        }
+    })
+
+}
+
+const deleteEmployeeById= (req,res)=> {
+    let pid = req.params.pid;
+    EmployeeModel.deleteOne({_id:pid},(err,result)=> {
+        if(!err){
+                if(result.deletedCount>0){
+                    res.send("Record deleted successfully")
+                }else {
+                    res.send("Record not present");
+                }
+        }else {
+            res.send("Error generated "+err);
+        }
+    })
+
+}
+
+module.exports = {empSignIn,empSignUp,addEmployeeInfo,deleteEmployeeById}
